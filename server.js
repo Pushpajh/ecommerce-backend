3dcf611b756ce5c62c59f0f0bc007dd1a492e79c
Pushpajh <pushpahosamani50@gmail.com>
2025-11-12 21:04:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -31,6 +31,6 @@ mongoose
   .catch((err) => console.log("❌ MongoDB Connection Error:", err.message));
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
